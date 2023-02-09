f9565114be33f03419f13f3c0e4edbdfdf72ce98
Piotr <piotr@boundlesshq.com>
2023-02-09 10:56:39 +0000

    Fix order

--- lib/active_hash/relation.rb
+++ lib/active_hash/relation.rb
@@ -12,6 +12,7 @@ module ActiveHash
       self.all_records = all_records
       self.query_hash = query_hash
       self.records_dirty = false
+      self.order_values = []
       self
     end
 
@@ -94,7 +95,14 @@ module ActiveHash
     end
 
     def reload
-      @records = filter_all_records_by_query_hash
+      filtered_records = filter_all_records_by_query_hash
+
+      if order_values.present?
+        filtered_records = filtered_records.dup if filtered_records.equal? all_records
+        order_by_args!(filtered_records)
+      end
+
+      @records = filtered_records
     end
 
     def order(*options)
@@ -102,10 +110,8 @@ module ActiveHash
       relation = where({})
       return relation if options.blank?
 
-      processed_args = preprocess_order_args(options)
       candidates = relation.dup
-
-      order_by_args!(candidates, processed_args)
+      candidates.order_values.concat preprocess_order_args(options)
 
       candidates
     end
@@ -120,11 +126,11 @@ module ActiveHash
       instance_exec(*args, &self.klass.scopes[method_name])
     end
 
-    attr_reader :query_hash, :klass, :all_records, :records_dirty
+    attr_reader :query_hash, :klass, :all_records, :records_dirty, :order_values
 
     private
 
-    attr_writer :query_hash, :klass, :all_records, :records_dirty
+    attr_writer :query_hash, :klass, :all_records, :records_dirty, :order_values
 
     def records
       if !defined?(@records) || @records.nil? || records_dirty
@@ -188,8 +194,8 @@ module ActiveHash
       ary.map! { |e| e.split(/\W+/) }.reverse!
     end
 
-    def order_by_args!(candidates, args)
-      args.each do |arg|
+    def order_by_args!(candidates)
+      order_values.each do |arg|
         field, dir = if arg.is_a?(Hash)
                        arg.to_a.flatten.map(&:to_sym)
                      elsif arg.is_a?(Array)
